66628374de1abb9e7640b45967d1cdd51d63d622
Yasassri Ratnayake <ycrnet@gmail.com>
2017-06-01 21:19:58 +0530

    Commiting Scripts to automate Infrastructure Creation

---
+++ cluster-create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#This file is invokes via init.sh
+##Create infrastructure and inventory file
+log "===Infrastructure preperation script - cluster-create.sh Initiated==="
+log "Start creating infrastructure with Terraform"
+
+terraform apply 
+
+##Run Ansible configurations
+log "Quick sleep while instances start and SSH is ready to serve"
+sleep 60
+log "Ansible provisioning started"
+export ANSIBLE_HOST_KEY_CHECKING=False
+ansible-playbook -i $script_path/kargo/inventory/inventory -u core -b $script_path/kargo/cluster.yml
+
+
---
+++ cluster-destroy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "destroying the Kubenetes cluster and associated instances" 
+terraform destroy -force
